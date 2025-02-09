93f2f345d110466dad55549d02fbe9eb83e89292
Oshadhi Wickramasinghe <117194835+oshadhi33@users.noreply.github.com>
2025-02-10 00:31:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple JavaScript for adding items to cart
+function addToCart(productName) {
+    alert(`${productName} has been added to your cart!`);
+    // You can expand this function to handle a shopping cart logic
+  }
